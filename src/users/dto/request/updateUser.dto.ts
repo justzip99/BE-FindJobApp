21c8728179b3f2683d0b32312ed08fe0fdf3d4e0
tini2 <datndt@smartosc.com>
2024-09-27 11:31:11 +0700

    Implement decorator for datatype

--- src/users/dto/request/updateUser.dto.ts
+++ src/users/dto/request/updateUser.dto.ts
@@ -1,6 +1,15 @@
-export class updateUser {
+import { IsEmail, IsNumber, IsString } from 'class-validator';
+
+export class UpdateUser {
+  @IsString()
   userName: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
   resume: string;
+
+  @IsNumber()
   balance: number;
 }
